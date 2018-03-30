c21a2ff91d06771ba7ae51e4e30b1ba79566c815
Sebastian Bastidas <sbastidasr@gmail.com>
2018-03-30 10:00:36 -0600

    fixes

--- src/components/pages/goals-page/todo-goals.test.js
+++ src/components/pages/goals-page/todo-goals.test.js
@@ -1,55 +1,61 @@
-import React from 'react';
-import {shallow, mount} from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 
 import TodoGoals from "./todo-goals";
 //will renders title and all be included here or goalspage-test?
-describe('<TodoGoals />', () => {
-	const seedGoals = [];
-		beforeAll(() => {
-			for(let i=0; i<10; i++) {
-				seedGoals.push({
-					text: `Goal ${i}`
-				})
-			}
-		});
 
-	it('Renders without crashing', () => {
-		shallow(<TodoGoals />);
-	});
+describe("<TodoGoals />", () => {
+  const seedGoals = [];
+  beforeAll(() => {
+    for (let i = 0; i < 10; i++) {
+      seedGoals.push({
+        description: `Goal ${i}`,
+        category: "Sports",
+        created: Date.now(),
+        due: Date.now(),
+        checked: true,
+        completed: Date.now()
+      });
+    }
+  });
 
-	it('Renders the title', () => {
-		const title = "Foo";
-		const wrapper = shallow(<TodoGoals title={title} goals={[]} />);
-		expect(wrapper.contains({title})).toEqual(true);
-	});
+  it("Renders without crashing", () => {
+    shallow(<TodoGoals goals={[]} />);
+  });
 
-	it('Renders the goals', () => {
-		const wrapper = shallow(<TodoGoals goals={seedGoals} />);
-		const goals = wrapper.find(Goal);
-		expect(goals.length).toEqual(seedGoals.length);
-		const firstGoal = goals.first();
-		expect(firstGoal.prop('text')).toEqual(seedGoals[0].text);
-	});
+  it("Renders the title", () => {
+    const title = "Foo";
+    const wrapper = shallow(<TodoGoals title={title} goals={[]} />);
+    expect(wrapper.contains(title)).toEqual(true);
+  });
 
-	// it('Should dispatch putGoal when goals goals are toggled', () => {
-	//     const dispatch = jest.fn();
-	//     const wrapper = shallow(<TodoGoals dispatch={dispatch} goals={[]} />);
-	//     const link = wrapper.find(toggleChecked());
-	//     link.simulate('click');
-	//     expect(dispatch).toHaveBeenCalled();
-	//     const action = dispatch.mock.calls[0][0];
-	//     expect(action.type).toEqual(putGoal);
-	//   });
+  it("Renders the goals", () => {
+    const wrapper = shallow(<TodoGoals goals={seedGoals} />);
+    const goals = wrapper.find("");
+    expect(goals.length).toEqual(seedGoals.length);
+    const firstGoal = goals.first();
+    expect(firstGoal.prop("description")).toEqual(seedGoals[0].description);
+  });
 
-	it('Renders checked goals as Completed Goals', () => {
-		const wrapper = shallow(<TodoGoals goals={[]} />);
-		// const completedGoals = wrapper.find(checked);
-		// expect(wrapper.contains({completedGoals})).toEqual(true);
-	});
+  // it('Should dispatch putGoal when goals goals are toggled', () => {
+  //     const dispatch = jest.fn();
+  //     const wrapper = shallow(<TodoGoals dispatch={dispatch} goals={[]} />);
+  //     const link = wrapper.find(toggleChecked());
+  //     link.simulate('click');
+  //     expect(dispatch).toHaveBeenCalled();
+  //     const action = dispatch.mock.calls[0][0];
+  //     expect(action.type).toEqual(putGoal);
+  //   });
 
-	it('Renders unchecked goals as Todo Goals', () => {
-		const wrapper = shallow(<TodoGoals goals={[]} />);
-		// const todoGoals = wrapper.find(!checked);
-		// expect(wrapper.const({todoGoals})).toEqual(true);
-	});
+  it("Renders checked goals as Completed Goals", () => {
+    const wrapper = shallow(<TodoGoals goals={[]} />);
+    // const completedGoals = wrapper.find(checked);
+    // expect(wrapper.contains({completedGoals})).toEqual(true);
+  });
+
+  it("Renders unchecked goals as Todo Goals", () => {
+    const wrapper = shallow(<TodoGoals goals={[]} />);
+    // const todoGoals = wrapper.find(!checked);
+    // expect(wrapper.const({todoGoals})).toEqual(true);
+  });
 });
