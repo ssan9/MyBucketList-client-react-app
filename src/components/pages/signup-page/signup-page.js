e4e53660b2014a78b750989b8ae1d7db4ee17924
Sebastian Bastidas <sbastidasr@gmail.com>
2018-04-10 11:53:25 -0400

    Redux form on signup

--- src/components/pages/signup-page/signup-page.js
+++ src/components/pages/signup-page/signup-page.js
@@ -1,25 +1,48 @@
 import React from "react";
 import { connect } from "react-redux";
 // import { Link, Redirect } from "react-router-dom";
-
+import { Field, reduxForm, focus } from "redux-form";
+import { registerUser } from "../../../actions/users";
+import { login } from "../../../actions/auth";
+import Input from "../../input";
+import {
+  required,
+  nonEmpty,
+  matches,
+  length,
+  isTrimmed
+} from "../../../validators";
 import "./signup-page.css";
 
-export function SignupPage(props) {
-  // If we are logged in redirect straight to the user's dashboard
-  // if (props.loggedIn) {
-  //   return <Redirect to="/dashboard" />;
-  // }
+const passwordLength = length({ min: 10, max: 72 });
+const matchesPassword = matches("password");
 
-  return (
-    <div className="signup-form">
-      <h2 className="title">Signup</h2>
-        <form className="register-form">
-            <div className="top-row">
+export class SignupPage extends React.Component {
+  onSubmit(values) {
+    console.log(values);
+    const { username, password, firstName, lastName } = values;
+    const user = { username, password, firstName, lastName };
+    return this.props
+      .dispatch(registerUser(user))
+      .then(() => this.props.dispatch(login(username, password)));
+  }
+
+  render() {
+    return (
+      <div className="signup-form">
+        <h2 className="title">Signup</h2>
+
+        <form
+          className="register-form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+        >
+          <div className="top-row">
             <div className="fields">
-              <input 
-                type="text" 
-                name="first-name" 
-                id="first-name" 
+              <Field
+                component={Input}
+                type="text"
+                name="firstName"
+                id="first-name"
                 className="name register"
                 autoComplete="off"
                 aria-labelledby="First Name"
@@ -28,67 +51,83 @@ export function SignupPage(props) {
               />
             </div>
             <div className="fields">
-              <input 
+              <Field
+                component={Input}
                 type="text"
-                name="last-name"
+                name="lastName"
                 id="last-name"
                 className="name register"
                 autoComplete="off"
                 aria-labelledby="Last Name"
                 placeholder="Last Name"
                 required
-              /> 
-            </div>
-            </div>
-            <div className="fields">
-              <input 
-                type="text"
-                name="username"
-                id="username"
-                className="register"
-                autoComplete="off"
-                aria-labelledby="Username"
-                placeholder="Username"
-                required
               />
             </div>
-            <div className="fields">
-              <input 
-                type="email"
-                name="email"
-                id="email"
-                className="register auth"
-                autoComplete="off"
-                aria-labelledby="Email address"
-                placeholder="Email Address"
-                required
-              />
-            </div>
-            <div className="fields">
-              <input
-                type="text"
-                name="password"
-                id="password"
-                className="register auth"
-                autoComplete="off"
-                aria-labelledby="Password"
-                placeholder="Password"
-                required
-              />
-            </div>
+          </div>
+          <div className="fields">
+            <Field
+              component={Input}
+              type="text"
+              name="username"
+              id="username"
+              className="register"
+              autoComplete="off"
+              aria-labelledby="Username"
+              placeholder="Username"
+              required
+            />
+          </div>
 
-          <h4 className="terms">By Signing up you agree to the Terms and Conditions</h4>
+          <div className="fields">
+            <Field
+              component={Input}
+              type="email"
+              name="email"
+              id="email"
+              className="register auth"
+              autoComplete="off"
+              aria-labelledby="Email address"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+
+          <div className="fields">
+            <Field
+              component={Input}
+              type="text"
+              name="password"
+              id="password"
+              className="register auth"
+              autoComplete="off"
+              aria-labelledby="Password"
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <h4 className="terms">
+            By Signing up you agree to the Terms and Conditions
+          </h4>
 
           <button type="submit" className="create-account">
-              CREATE ACCOUNT
+            Register
           </button>
-      </form>
-    </div>
-  );
+        </form>
+      </div>
+    );
+  }
 }
+//
+// const mapStateToProps = state => ({
+//   loggedIn: state.auth.currentUser !== null
+// });
 
-const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
-});
+export default reduxForm({
+  form: "registration",
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus("registration", Object.keys(errors)[0]))
+})(SignupPage);
 
-export default connect(mapStateToProps)(SignupPage);
+//
+// export default connect(mapStateToProps)(SignupPage);
