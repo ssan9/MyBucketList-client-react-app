a2d263055e4880eeb5ba11628614d56efc30b62d
ssan9 <perrsy@gmail.com>
2018-05-08 12:23:06 -0400

    removed unused code

--- src/components/pages/reusable-components/navigation-bar.js
+++ src/components/pages/reusable-components/navigation-bar.js
@@ -1,27 +1,15 @@
 import React from "react";
 
-import { Link } from "react-router-dom"; //Redirect was also there after comma
+import { Link } from "react-router-dom"; 
 import "./navigation-bar.css";
-import { landingLinks, links } from "./nav-links-data"; //title was also there as the first component
+import { landingLinks, links } from "./nav-links-data"; 
 import {clearAuth} from "../../../actions/auth";
 import {clearAuthToken} from "../../../local-storage";
 import { connect } from "react-redux";
 
 export function NavigationBar(props) {
-  // let displayLinks = landingLinks;
-  // const linkElements = displayLinks.map((link, index) => {
-  //   return (
-  //     <li key={index}>
-  //       <Link to={link.href}>
-  //         {link.text}
-  //       </Link>
-
-  //     </li>
-  //   );
-  // });
 
   let displayLinks = props.loggedIn ? links: landingLinks;
-  //  let displayLinks = landingLinks;
   const linkElements = displayLinks.map((link, index) => {
     return (
       <li key={index}>
@@ -33,53 +21,18 @@ export function NavigationBar(props) {
     );
   })
 
-
-  // let displayLink = links;
-  // const displayElements = displayLink.map((link, index) => {
-  //   console.log(props.loggedIn)
-             
-  //         if(props.loggedIn) {
-
-  //       return (
-  //         <li key={index}>
-  //           <Link to={link.href}>
-  //             {link.text}
-  //           </Link>
-
-  //         </li>
-  //       )
-
-  //       else {
-  //           {linkElements}
-  //       }
-      
-  //   }
-  // })
-  
-
-  
-
-
-
-  
-  // if(props.loggedIn) {
-  //   {displayElements}
-  // }
-
- 
-  
-      
   function logOut() {
-        props.dispatch(clearAuth());
-        clearAuthToken();
-    }
+    props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
   let logOutButton;
-        if (props.loggedIn) {
-          console.log(props.loggedIn);
-            logOutButton = (
-                <button className="logout" onClick={() => logOut()}>Log out</button>
-            );
-        }
+    if (props.loggedIn) {
+      console.log(props.loggedIn);
+        logOutButton = (
+            <button className="logout" onClick={() => logOut()}>Log out</button>
+        );
+    }
   return (
     <nav className="navigation-bar">
       <h1 className="current" id="app-name">
--- src/components/pages/reusable-components/navigation-bar.test.js
+++ src/components/pages/reusable-components/navigation-bar.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {shallow} from 'enzyme';
-
 import {NavigationBar} from "./navigation-bar"
 
 describe('<NavigationBar />', () => {
